c0ce17b1be043efbb5c0a30781011ab11aba8671
Iraniya Naynesh <iraniyanaynesh@gmail.com>
2018-07-05 16:39:18 +0530

    color ball changes

--- ColorBallView/ViewController.swift
+++ ColorBallView/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorBallView: ColorBallView!
-    
+    @IBOutlet weak var tempColorBox: UIView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,9 +29,18 @@ class ViewController: UIViewController {
 }
 
 
+//MARK: Glo Color view Delegates
+
 extension ViewController: ColorBallViewDelegate {
-    func colorBallView(_ colorBall: ColorBallView, andSelectedColor color: UIColor, withBrightness brightness: Int) {
-        print(color)
-        print(brightness)
+    func colorBallView(_ colorBall: ColorBallView, withColorValues colorsValues: (Int, Int, Int, Int), withBrightness brightness: Int, userInteractionFinished: Bool, hueAngle: Double, brightnessAngle: Double) {
+        
+        print("colorsValues \(colorsValues)")
+        print("brightness \(brightness)")
+        print("userInteractionFinished \(userInteractionFinished)")
+        print("hueAngle \(hueAngle)")
+        print("brightnessAngle \(brightnessAngle)")
+        let color: UIColor = UIColor(red: CGFloat(colorsValues.0)/255.0, green: CGFloat(colorsValues.1)/255.0, blue: CGFloat(colorsValues.2)/255.0, alpha: CGFloat(colorsValues.3)/255.0)
+        
+        tempColorBox.backgroundColor = color
     }
 }
